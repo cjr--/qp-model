da4f97b5e1b0040148443c98bcee051ef3e143fe
Chris Richardson <cjr@hotmail.co.uk>
2017-10-20 09:37:37 +0100

    project init

---
+++ index.js
@@ -0,0 +1,25 @@
+define(module, function(exports, require) {
+
+  var qp = require('qp-utility');
+
+  qp.make(exports, {
+
+    ns: 'qp-model',
+
+    create: function(schema, data) {
+      schema.create(data, { instance: this });
+    },
+
+    refresh: function() { },
+
+    set_user: function(user) {
+      if (qp.is(user, 'array')) user = qp.find(user, { id: this.user_id });
+      if (qp.is(user, 'user')) {
+        this.user = user;
+        this.user_id = this.user.id;
+      }
+    }
+
+  });
+
+});
---
+++ schema.js
@@ -0,0 +1,93 @@
+define(module, function(exports, require) {
+
+  var qp = require('qp-utility');
+
+  function default_string() { return ''; }
+  function default_number() { return 0; }
+  function default_boolean() { return false; }
+  function default_datetime() { return new Date(); }
+  function default_date() {
+    var date = new Date();
+    date.setUTCHours(12, 0, 0, 0);
+    return date;
+  }
+
+  qp.module(exports, {
+
+    ns: 'qp-library/schema',
+
+    build: function(_exports, schema) {
+      schema.create = this.create.bind(this, schema.columns);
+      schema.table = { name: schema.table };
+      schema.fields = { managed: [], all: [] };
+      qp.each_own(schema.columns, function(column, name) {
+        column.name = name;
+        if (column.managed) schema.fields.managed.push(name); else schema.fields.all.push(name);
+      });
+      _exports(schema);
+    },
+
+    create: function(fields, data, options) {
+      data = data || {};
+      options = qp.options(options, { internal: false });
+      var o = options.instance || {};
+      qp.each_own(fields, function(v, k) {
+        if (options.internal || !v.internal) {
+          o[k] = data[k] || v.default();
+        }
+      });
+      return o;
+    },
+
+    field: function(type, size, options) {
+      if (qp.is(size, 'object')) options = size; size = 0;
+      var field;
+      if (type === 'text') {
+        if (size) {
+          field = { type: 'varchar', size: size, default: default_string };
+        } else {
+          field = { type: 'text', default: default_string };
+        }
+      } else if (type === 'int' || type === 'integer') {
+        if (size === 2) {
+          field = { type: 'smallint', default: default_number };
+        } else if (size === 8) {
+          field = { type: 'bigint', default: default_number };
+        } else {
+          field = { type: 'integer', default: default_number };
+        }
+      } else if (type === 'bool' || type === 'boolean') {
+        field = { type: 'boolean', default: default_boolean };
+      } else if (type === 'datetime') {
+        field = { type: 'date', default: default_datetime };
+      } else if (type === 'date') {
+        field = { type: 'date', default: default_date };
+      } else {
+        field = { type: '', default: qp.noop };
+      }
+      return qp.options(field, options);
+    },
+
+    primary: function() {
+      return this.field('int', 64, { primary: true, managed: true });
+    },
+
+    foreign: function(table) {
+      return this.field('int', 64, { foreign: true, table: table });
+    },
+
+    foreign_ids: function(table) {
+      return this.field('int', 64, { foreign: true, table: table, array: true });
+    },
+
+    created: function() {
+      return this.field('datetime', { managed: true });
+    },
+
+    modified: function() {
+      return this.field('datetime', { managed: true });
+    }
+
+  });
+
+});
