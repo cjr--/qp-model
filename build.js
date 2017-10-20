a5a125ec0f64a0c333b3fc7cde7bcbeb684c8b53
Chris Richardson <cjr@hotmail.co.uk>
2017-10-20 19:40:45 +0100

    build process

---
+++ build.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var fs = require('fs');
+var uglify = require('uglify-js');
+
+fs.writeFileSync(
+  path.join(__dirname, 'qp-model.js'), [
+    fs.readFileSync(path.join(__dirname, 'index.js'), 'utf8'),
+    fs.readFileSync(path.join(__dirname, 'schema.js'), 'utf8')
+  ].join('\n')
+);
+
+fs.writeFileSync(
+  path.join(__dirname, 'qp-model.min.js'),
+  uglify.minify(fs.readFileSync(path.join(__dirname, 'qp-model.js'), 'utf8')).code
+);
