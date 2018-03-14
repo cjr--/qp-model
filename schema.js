260b80219e8e4f570b6fef2c23d3cb6e9d36e894
cjr-- <cjr@hotmail.co.uk>
2018-03-14 09:54:18 +0000

    schema; improve meta data support for display column widths

--- schema.js
+++ schema.js
@@ -24,6 +24,30 @@ define(module, function(exports, require) {
         schema.columns[name] = column;
         if (column.managed) schema.fields.managed.push(name); else schema.fields.all.push(name);
       });
+      qp.each(o.meta.columns, function(meta_column, i) {
+        var column = schema.columns[meta_column.name];
+        column.index = i;
+        if (meta_column.width !== 0) {
+          column.width = meta_column.width;
+        } else if (column.boolean) {
+          column.width = 6;
+        } else if (column.array) {
+          column.width = 30;
+        } else if (column.primary || column.primary_key || column.foreign) {
+          column.width = 6;
+        } else if (column.datetime) {
+          column.width = 24;
+        } else if (column.int) {
+          column.width = ({ 2: 6, 4: 11, 8: 20 })[column.size];
+        } else if (column.numeric) {
+          column.width = column.size + column.scale + 1;
+        } else if (column.text) {
+          column.width = column.size || 30;
+        } else if (column.data) {
+          column.width = 30;
+        }
+        column.width = Math.max(column.name.length, column.width);
+      });
       _exports(schema);
     },
 
@@ -111,41 +135,41 @@ define(module, function(exports, require) {
       var field;
 
       if (type === 'text' && size === 0) {
-        field = { type: 'varchar', size: size, default: options.default || default_string };
+        field = { type: 'text', text: true, size: 0, default: options.default || default_string };
       } else if (type === 'text') {
-        field = { type: 'text', default: options.default || default_string };
+        field = { type: 'varchar', text: true, size: size, default: options.default || default_string };
 
       } else if (type === 'smallint' || type === 'int2') {
-        field = { type: 'smallint', size: 2, default: options.default || default_number };
+        field = { type: 'smallint', int: true, size: 2, default: options.default || default_number };
       } else if (type === 'int' || type === 'int4' || type === 'integer') {
-        field = { type: 'integer', size: 4, default: options.default || default_number };
+        field = { type: 'integer', int: true, size: 4, default: options.default || default_number };
       } else if (type === 'bigint' || type === 'int8') {
-        field = { type: 'bigint', size: 8, default: options.default || default_number };
+        field = { type: 'bigint', int: true, size: 8, default: options.default || default_number };
 
       } else if (type === 'numeric' || type === 'decimal' || type === 'number') {
-        field = { type: 'numeric', size: size, scale: scale, default: options.default || default_number };
+        field = { type: 'numeric', numeric: true, size: size, scale: scale, default: options.default || default_number };
 
       } else if (type === 'currency') {
-        field = { type: 'numeric', size: 12, scale: 4, default: options.default || default_number };
+        field = { type: 'numeric', numeric: true, size: 12, scale: 4, default: options.default || default_number };
 
       } else if (type === 'bool' || type === 'boolean') {
-        field = { type: 'boolean', default: options.default || default_boolean };
+        field = { type: 'boolean', boolean: true, default: options.default || default_boolean };
 
       } else if (type === 'dt' || type === 'datetime') {
-        field = { type: 'timestamp with time zone', default: options.default || default_datetime };
+        field = { type: 'timestamp with time zone', datetime: true, default: options.default || default_datetime };
 
       } else if (type === 'date') {
-        field = { type: 'timestamp with time zone', default: options.default || default_date };
+        field = { type: 'timestamp with time zone', date: true, default: options.default || default_date };
 
       } else if (type === 'bytea') {
-        field = { type: 'bytea', default: options.default || default_bytea };
+        field = { type: 'bytea', data: true, default: options.default || default_bytea };
 
       } else if (type === 'smallserial') {
-        field = { type: 'smallserial', default: qp.noop };
+        field = { type: 'smallserial', int: true, size: 2, default: qp.noop };
       } else if (type === 'serial') {
-        field = { type: 'serial', default: qp.noop };
+        field = { type: 'serial', int: true, size: 4, default: qp.noop };
       } else if (type === 'bigserial') {
-        field = { type: 'bigserial', default: qp.noop };
+        field = { type: 'bigserial', int: true, size: 8, default: qp.noop };
 
       } else {
         field = { type: '', default: qp.noop };
