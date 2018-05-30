35804f21f4824fb55a664746d86b54855ff2360c
cjr-- <cjr@hotmail.co.uk>
2018-05-30 07:36:05 +0100

    model; member management, clone fn & state placeholder

--- qp-model.js
+++ qp-model.js
@@ -6,25 +6,41 @@ define(module, function(exports, require) {
 
     ns: 'qp-model/model',
 
+    $state: null,
+    $members: null,
+
     init: function(o) {
       var data = qp.delete_key(o, this.schema.name);
       this.set_data(data, o);
-      if (o && o.display) {
-        this.initialise({ create: o.create });
-        this.refresh();
-      }
+      this.$state = { created: o.create || false, modified: false, deleted: false };
+      this.$members = [];
+      if (o && o.display) this.initialise({ create: o.create });
+      if (o && o.members) this.set_members(o.members);
+      if (o && o.display) this.refresh();
     },
 
     initialise: function(o) { },
     refresh: function() { },
 
-    set_user: function(user) {
-      if (qp.is(user, 'array')) user = qp.find(user, { id: this.user_id });
-      if (qp.is(user, 'user')) {
-        this.user = user;
-        this.user_id = user.id;
-      }
+    set_members: function(members) {
+      qp.each_own(members, function(member, name) {
+        this['set_' + name](member);
+      }.bind(this));
     },
+
+    set_member: function(name, model, id_name, id_key) {
+      id_name = id_name || 'id';
+      id_key = id_key || (name + '_' + id_name);
+      var key_value = this[id_key];
+      if (qp.is(model, 'array')) model = qp.find(model, function(o) { return o[id_name] === key_value; });
+      if (qp.is(model, 'object')) {
+        this[name] = model;
+        this[id_key] = model[id_name];
+      }
+      this.$members.push(name);
+    },
+
+    set_user: function(user) { this.set_member('user', user); },
 
     set_data: function(source, options) {
       qp.each(this.schema.columns, function(column) {
@@ -56,6 +72,16 @@ define(module, function(exports, require) {
         }
       }.bind(this));
       return target;
+    },
+
+    clone: function() {
+      var config = { display: true };
+      config[this.schema.name] = this.get_data();
+      var clone = this.self.create(config);
+      qp.each(this.members, function(name) {
+        var member = this[name];
+        if (member && member.clone) clone['set_' + name](member.clone());
+      }.bind(this));
     }
 
   });
